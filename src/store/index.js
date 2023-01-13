c81b7c57045edbaacee733a4c297301d6d7bb7ce
Imran Ansari <i4imran322@gmail.com>
2023-01-13 21:33:14 +0530

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const moviesSlice = createSlice({
-  name: "Movie",
+  name: "movie",
   initialState: [],
   reducers: {
     addMovie(state, action) {
@@ -10,6 +10,9 @@ const moviesSlice = createSlice({
     removeMovie(state, action) {
       const index = state.indexOf(action.payload);
       state.splice(index, 1);
+    },
+    reset(state, action) {
+      return [];
     },
   },
 });
@@ -26,6 +29,11 @@ const songsSlice = createSlice({
       state.splice(index, 1);
     },
   },
+  extraReducers(builder) {
+    builder.addCase(moviesSlice.actions.reset, (state, action) => {
+      return [];
+    });
+  },
 });
 
 const store = configureStore({
@@ -37,4 +45,4 @@ const store = configureStore({
 
 export { store };
 export const { addSong, removeSong } = songsSlice.actions;
-export const { addMovie, removeMovie } = moviesSlice.actions;
+export const { addMovie, removeMovie, reset } = moviesSlice.actions;
